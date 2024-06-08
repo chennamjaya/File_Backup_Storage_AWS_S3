e90139e4bb0aed97c62717de6033d6816d428314
chennamjaya <chennamjaya@gmail.com>
2024-06-08 21:13:16 +0100

    Updated the file name

--- backup.sh
+++ backup.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-SOURCE_DIR="C:/Users/vaishnavi/Desktop/Jaya_Vaishnavi_AWS_Devops.docx"
+
+# Define the directory to back up
+SOURCE_DIR="/mnt/c/Users/vaishnavi/Desktop"  # Use the directory containing the file
+
+# Define the specific file to back up
+FILE_NAME="Jaya_Vaishnavi_AWS_Devops.docx"
+
+# Define the S3 bucket name
 BUCKET_NAME="aws-backup-bucket-files"
+
+# Generate a timestamp
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
+# Define the backup file name
 BACKUP_NAME="backup-$TIMESTAMP.tar.gz"
 
-# Create a compressed archive of the source directory
-tar -czf /tmp/$BACKUP_NAME -C $SOURCE_DIR .
+# Create a compressed archive of the specific file
+tar -czf /tmp/$BACKUP_NAME -C $SOURCE_DIR $FILE_NAME
 
 # Upload the backup to S3
 aws s3 cp /tmp/$BACKUP_NAME s3://$BUCKET_NAME/$BACKUP_NAME
