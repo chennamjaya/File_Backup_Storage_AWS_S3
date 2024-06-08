f388d252e6f31507e4a06b2f1a818325d6030be7
chennamjaya <chennamjaya@gmail.com>
2024-06-08 21:50:37 +0100

    Updated the MIME type

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the directory to back up
-SOURCE_DIR="/mnt/c/Users/vaishnavi/Desktop"  # Use the directory containing the file
+SOURCE_DIR="/mnt/c/Users/vaishnavi/Desktop"
 
 # Define the specific file to back up
 FILE_NAME="Jaya_Vaishnavi_AWS_Devops.docx"
@@ -16,10 +16,33 @@ TIMESTAMP=$(date +%Y%m%d%H%M%S)
 BACKUP_NAME="backup-$TIMESTAMP.tar.gz"
 
 # Create a compressed archive of the specific file
-tar -czf /tmp/$BACKUP_NAME -C $SOURCE_DIR $FILE_NAME
+tar -czf /tmp/$BACKUP_NAME -C "$SOURCE_DIR" "$FILE_NAME"
 
-# Upload the backup to S3
-aws s3 cp /tmp/$BACKUP_NAME s3://$BUCKET_NAME/$BACKUP_NAME
+# Determine the MIME type based on the file extension
+EXTENSION="${FILE_NAME##*.}"
+case "$EXTENSION" in
+    txt)
+        MIME_TYPE="text/plain"
+        ;;
+    docx)
+        MIME_TYPE="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        ;;
+    pdf)
+        MIME_TYPE="application/pdf"
+        ;;
+    jpg|jpeg)
+        MIME_TYPE="image/jpeg"
+        ;;
+    png)
+        MIME_TYPE="image/png"
+        ;;
+    *)
+        MIME_TYPE="application/octet-stream"
+        ;;
+esac
+
+# Upload the backup to S3 with the correct MIME type
+aws s3 cp /tmp/$BACKUP_NAME s3://$BUCKET_NAME/$BACKUP_NAME --content-type "$MIME_TYPE"
 
 # Remove the local backup file
 rm /tmp/$BACKUP_NAME
