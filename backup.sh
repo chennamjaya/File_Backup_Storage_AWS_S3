74c1db58ed8081364df3ccc332f93fe6b1726db0
chennamjaya <chennamjaya@gmail.com>
2024-06-08 18:49:38 +0100

    Updated the file name

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SOURCE_DIR="C:\Users\vaishnavi\Desktop\Jaya_Vaishnavi_AWS_Devops.docx"
+SOURCE_DIR="C:/Users/vaishnavi/Desktop/Jaya_Vaishnavi_AWS_Devops.docx"
 BUCKET_NAME="aws-backup-bucket-files"
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
 BACKUP_NAME="backup-$TIMESTAMP.tar.gz"
