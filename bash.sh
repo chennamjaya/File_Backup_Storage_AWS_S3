aa59a8dee253827095a945dd98d9603dfe4bd702
chennamjaya <chennamjaya@gmail.com>
2024-06-08 17:31:33 +0100

    Add backup script

---
+++ bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SOURCE_DIR="/path/to/your/files"
+BUCKET_NAME="my-backup-bucket"
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+BACKUP_NAME="backup-$TIMESTAMP.tar.gz"
+
+# Create a compressed archive of the source directory
+tar -czf /tmp/$BACKUP_NAME -C $SOURCE_DIR .
+
+# Upload the backup to S3
+aws s3 cp /tmp/$BACKUP_NAME s3://$BUCKET_NAME/$BACKUP_NAME
+
+# Remove the local backup file
+rm /tmp/$BACKUP_NAME
